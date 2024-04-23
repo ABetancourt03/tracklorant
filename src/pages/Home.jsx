ff183540431f2390519f1f8ff4d8d7c8b07e839f
ABetancourt03 <neutralzgamer@gmail.com>
2024-04-23 19:07:14 -0400

    Add header to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,48 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
+
+import Header from '../components/Header'
+import { Link } from 'react-router-native'
 
 export default function Home () {
   return (
-    <View>
-      <Text>Home Page</Text>
+    <View style={styles.container}>
+      <Header>
+        <Image
+          style={{
+            width: 48,
+            height: 48
+          }}
+          source={require('../icons/logo.png')}
+        />
+      </Header>
+
+      <View style={styles.content}>
+        <Text style={styles.title}>Patch Notes</Text>
+        <Link to='/signin'>
+          <Text>SignIn</Text>
+        </Link>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1B242E'
+  },
+  content: {
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
+  },
+  title: {
+    color: '#FEF7EE',
+    fontFamily: 'Roboto',
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginTop: 25
+  },
+  logo: {
+  }
+})
