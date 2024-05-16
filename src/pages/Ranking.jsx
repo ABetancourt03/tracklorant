d3b6c6ba62a1b7fe5af69323216cb1c312ec75bf
ABetancourt03 <neutralzgamer@gmail.com>
2024-05-16 12:01:07 -0400

    Add url and apikey

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,8 +1,27 @@
-import { View, StyleSheet, Image } from 'react-native'
+import { useEffect, useState } from 'react'
+import { View, SafeAreaView, FlatList, StyleSheet, Image, Text } from 'react-native'
 import Header from '../components/Header'
 import Navigation from '../components/Navigation'
 
+import { RIOT_API_KEY } from '@env'
+
+const act = '4539cac3-47ae-90e5-3d01-b3812ca3274e'
+const url = `https://latam.api.riotgames.com/val/ranked/v1/leaderboards/by-act/${act}?size=100&startIndex=0&api_key=${RIOT_API_KEY}`
+
 export default function Ranking () {
+  const [ranking, setRanking] = useState([])
+
+  const getRanking = async () => {
+    const res = await fetch(url)
+    const data = res.json()
+
+    setRanking(data)
+  }
+
+  useEffect(() => {
+    getRanking()
+  }, [])
+
   return (
     <View style={styles.container}>
       <Header text='Leaderboard'>
@@ -15,7 +34,13 @@ export default function Ranking () {
         />
       </Header>
 
-      <View style={styles.content} />
+      <SafeAreaView style={styles.content}>
+        <FlatList
+          data={ranking}
+          renderItem={({ item }) => <Text>{item.gameName}</Text>}
+          keyExtractor={item => item.puuid}
+        />
+      </SafeAreaView>
 
       <Navigation />
     </View>
